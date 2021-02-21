6c58c0211111ddae06fdf7363f27bbe0130e1f52
monica.lopez-gris <monica.lopez-gris@capgemini.com>
2021-02-21 19:54:02 +0100

    add ingress-srv

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('currentUser works fine');
+});
+
 app.listen(5000, () => {
   console.log('Listen on 5000');
 });
