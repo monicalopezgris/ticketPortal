13fc97652a9d2a5ce554f3a18616e4c4aafe9a34
monica.lopez-gris <monica.lopez-gris@capgemini.com>
2021-02-25 23:19:41 +0100

    add routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/currentUser';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('currentUser works fine');
-});
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
 
 app.listen(5000, () => {
   console.log('Listen on 5000');
