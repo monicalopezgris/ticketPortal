f2d2d8f73faf47aed931600681812a30ec4d1b16
monica.lopez.gris <monicalopezgris@gmail.com>
2021-08-22 17:02:07 +0200

    add user builder

---
+++ auth/src/models/user.ts
@@ -0,0 +1,34 @@
+import { Model, Schema, model } from "mongoose";
+
+interface UserData {
+  email: string;
+  password: string;
+}
+
+interface UserDoc {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends Model<UserDoc> {
+  build(data: UserData): UserDoc;
+}
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.statics.build = (data: UserData) => {
+  return new User(data);
+};
+
+const User = model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
