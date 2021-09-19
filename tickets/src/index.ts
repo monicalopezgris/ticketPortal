48f413edc5fa692359f1d37fd338285669dec1e5
monica.lopez-gris <monica.lopez-gris@capgemini.com>
2021-09-19 20:09:30 +0200

    update mongo conexions for auth and tickets

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,9 +5,12 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
 
   try {
-    await mongoose.connect("mongodb://ticket-mongo-srv:27017/auth", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
@@ -17,8 +20,8 @@ const start = async () => {
     console.error(error);
   }
 
-  app.listen(3001, () => {
-    console.log("Listening on port 3001!");
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!");
   });
 };
 
