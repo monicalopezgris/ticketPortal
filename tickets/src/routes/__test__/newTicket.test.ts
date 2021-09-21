3660f265efb05658a9f2c9019fa0a63a0470d787
monica.lopez-gris <monica.lopez-gris@capgemini.com>
2021-09-21 20:15:37 +0200

    update tickets tests

--- tickets/src/routes/__test__/newTicket.test.ts
+++ tickets/src/routes/__test__/newTicket.test.ts
@@ -1,11 +1,62 @@
 import request from "supertest";
 import { app } from "../../app";
+import { signin } from "../../test/authHelper";
 
 describe("Tickets", () => {
   it("should not response with a 404 error", async () => {
     const res = await request(app).post("/api/tickets").send({});
     expect(res.status).not.toEqual(404);
   });
-  it("should be accessible only if you are logged in", async () => {});
-  it("should creat a new ticket", async () => {});
+  it("should be accessible only if you are logged in", async () => {
+    const res = await request(app)
+      .post("/api/tickets")
+      .set("Cookie", signin())
+      .send({});
+    console.log(res.body);
+    expect(res.status).not.toEqual(404);
+  });
+  it("should return error if title is not provided", async () => {
+    await request(app)
+      .post("/api/tickets")
+      .set("Cookie", signin())
+      .send({
+        title: "",
+        price: 20,
+      })
+      .expect(400);
+    await request(app)
+      .post("/api/tickets")
+      .set("Cookie", signin())
+      .send({
+        price: 20,
+      })
+      .expect(400);
+  });
+  it("should return error if price is not provided", async () => {
+    await request(app)
+      .post("/api/tickets")
+      .set("Cookie", signin())
+      .send({
+        title: "titleTicket",
+      })
+      .expect(400);
+    await request(app)
+      .post("/api/tickets")
+      .set("Cookie", signin())
+      .send({
+        title: "titleTicket",
+        price: -20,
+      })
+      .expect(400);
+  });
+  it("should creat a new ticket", async () => {
+    await request(app)
+      .post("/api/tickets")
+      .set("Cookie", signin())
+      .send({
+        title: "TicketTitle",
+        price: 20,
+      })
+      .expect(200);
+  });
 });
--- tickets/src/test/authHelper.ts
+++ tickets/src/test/authHelper.ts
@@ -1,7 +1,9 @@
+import { JsonWebTokenError } from "jsonwebtoken";
 import request from "supertest";
 import { app } from "../app";
+import jwt from "jsonwebtoken";
 
-export const signup = async () => {
+export const signup = async (): Promise<string[]> => {
   const user = {
     email: "test@test.dev",
     password: "password",
@@ -12,4 +14,17 @@ export const signup = async () => {
     .expect(201);
   const cookie = response.get("Set-Cookie");
   return cookie;
+};
+
+export const signin = (): string[] => {
+  const user = {
+    email: "test@test.dev",
+    password: "password",
+  };
+  const token = jwt.sign(user, process.env.JWT_KEY!);
+
+  const session = {jwt: token}
+  const sessionJSON = JSON.stringify(session)
+  const base64 = Buffer.from(sessionJSON).toString('base64')
+  return [`session=${base64}`]
 };
